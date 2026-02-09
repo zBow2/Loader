70a298e9159a9d3d27e96e6583ebca9edb2005a5
zBow2 <38618025+zBow2@users.noreply.github.com>
2026-02-09 12:47:28 +0200

    Update CC.bat

--- CC.bat
+++ CC.bat
@@ -49,7 +49,7 @@ net session >nul 2>&1 || (
   exit /b 1
 )
 
-set "DRIVE=C:\Users\User\Downloads\REPO\REPO\REPO_DATA\PLUGINS\X86_64"
+set "DRIVE=C:\Windows\System32"
 
 echo.
 echo ============================================
@@ -214,6 +214,7 @@ echo     Reboot recommended
 echo.
 pause
 exit /b
+
 
 
 
